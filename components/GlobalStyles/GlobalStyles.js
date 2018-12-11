68d86ac723871c9968e87e87099e92900f919b2a
Drew Barontini <hello@drewb.io>
2018-12-11 18:18:03 -0500

    Add GlobalStyles

---
+++ components/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { palette } from '../../lib/styleUtils';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyles;
