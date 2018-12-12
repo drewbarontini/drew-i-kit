6bef585bad4a08ab49e8195d021f1a87c28deda7
Drew Barontini <hello@drewb.io>
2018-12-12 16:42:22 -0500

    Add Avatar component

---
+++ components/Avatar/Avatar.js
@@ -0,0 +1,35 @@
+// DEPENDENCY: <Image />
+
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import { ifProp } from 'styled-tools';
+
+import { theme, palette } from '../../lib/styleUtils';
+
+import Image from '../Image';
+
+const Avatar = styled(Image)`
+  ${ifProp(
+    'raised',
+    css`
+      border: 5px solid ${palette('bg')};
+      box-shadow: ${theme('shadows.base')};
+    `
+  )}
+
+  ${ifProp(
+    'rounded',
+    css`
+      border-radius: 100%;
+    `
+  )}
+`;
+
+Avatar.propTypes = {
+  raised: PropTypes.string,
+  rounded: PropTypes.string,
+};
+
+Avatar.displayName = 'Avatar';
+
+export default Avatar;
---
+++ components/Avatar/Avatar.story.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Avatar from './';
+
+const standardProps = {
+  src: 'https://source.unsplash.com/random/200x200',
+  alt: 'Unsplash Image',
+  width: '100',
+  height: '100',
+};
+
+storiesOf('Avatar', module)
+  .add('default', () => <Avatar {...standardProps} />)
+  .add('rounded', () => <Avatar rounded {...standardProps} />)
+  .add('raised', () => <Avatar raised {...standardProps} />)
+  .add('rounded and raised', () => (
+    <Avatar rounded raised {...standardProps} />
+  ));
