0320983d495b46f8c12d900d4302c262e149391a
Drew Barontini <hello@drewb.io>
2018-12-12 13:47:32 -0500

    Update Flex component

--- components/Flex/Flex.js
+++ components/Flex/Flex.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
+import { setProp } from '../../lib/styleUtils';
+
 const Flex = styled.div`
   display: flex;
+
+  ${setProp({ prop: 'alignItems', cssProp: 'align-items' })};
+  ${setProp({ prop: 'flex', cssProp: 'flex' })};
+  ${setProp({ prop: 'flexDirection', cssProp: 'flex-direction' })};
+  ${setProp({ prop: 'justifyContent', cssProp: 'justify-content' })};
+  ${setProp({ prop: 'flexWrap', cssProp: 'flex-wrap' })};
 `;
 
 Flex.displayName = 'Flex';
