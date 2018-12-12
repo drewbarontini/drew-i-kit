9dde31e418f8d050b97916fd334932c99076c6c1
Drew Barontini <hello@drewb.io>
2018-12-12 09:06:10 -0500

    Add Box story

---
+++ components/Box/Box.story.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Box from './';
+
+storiesOf('Box', module).add('default', () => (
+  <Box width="200px" height="200px" backgroundColor="primary" />
+));
