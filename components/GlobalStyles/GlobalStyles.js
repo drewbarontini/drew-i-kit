5fa6d1596abe3106d7a6b28c2262f3360970d7ba
Drew Barontini <hello@drewb.io>
2018-12-12 09:06:06 -0500

    Update GlobalStyles

--- components/GlobalStyles/GlobalStyles.js
+++ components/GlobalStyles/GlobalStyles.js
@@ -1,11 +1,33 @@
 import { createGlobalStyle } from 'styled-components';
 
-import { palette } from '../../lib/styleUtils';
+import { palette, theme } from '../../lib/styleUtils';
 
 const GlobalStyles = createGlobalStyle`
   * {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: ${palette('bg')};
+    color: ${palette('fg')};
+    font-family: ${theme('fonts.family.base')};
+    font-size: ${theme('fonts.sizes.base')};
+    line-height: ${theme('fonts.lineHeight.base')};
+    margin: ${theme('spacing.l')};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    margin-bottom: ${theme('spacing.s')};
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
   }
 `;
 
