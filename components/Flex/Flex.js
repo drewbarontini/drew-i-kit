93b63485a33c3cf6e499a468046f695cc47471e1
Drew Barontini <hello@drewb.io>
2018-12-12 15:58:18 -0500

    Update Flex component

--- components/Flex/Flex.js
+++ components/Flex/Flex.js
@@ -1,15 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { ifProp } from 'styled-tools';
 
 import { setProp } from '../../lib/styleUtils';
 
 const Flex = styled.div`
   display: flex;
 
+  ${ifProp(
+    'center',
+    css`
+      align-items: center;
+      justify-content: center;
+    `
+  )};
+
   ${setProp({ prop: 'alignItems', cssProp: 'align-items' })};
   ${setProp({ prop: 'flex', cssProp: 'flex' })};
   ${setProp({ prop: 'flexDirection', cssProp: 'flex-direction' })};
-  ${setProp({ prop: 'justifyContent', cssProp: 'justify-content' })};
   ${setProp({ prop: 'flexWrap', cssProp: 'flex-wrap' })};
+  ${setProp({ prop: 'height' })};
+  ${setProp({ prop: 'justifyContent', cssProp: 'justify-content' })};
 `;
 
 Flex.displayName = 'Flex';
