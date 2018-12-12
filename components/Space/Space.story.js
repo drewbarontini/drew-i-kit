95191970ad88f38ab1dbc5978dd16d194022eed7
Drew Barontini <hello@drewb.io>
2018-12-12 09:12:12 -0500

    Add Space story

---
+++ components/Space/Space.story.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Space from './';
+
+storiesOf('Space', module).add('default', () => (
+  <Fragment>
+    <Space bottom="s">
+      <h1>Hello, Space!</h1>
+    </Space>
+    <Space bottom="base">
+      <p>Here is some content.</p>
+    </Space>
+    <Space bottom="base">
+      <p>Here is some more content.</p>
+    </Space>
+  </Fragment>
+));
