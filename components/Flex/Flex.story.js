405eb0474cba2226aa7df42c46f082c794293322
Drew Barontini <hello@drewb.io>
2018-12-12 09:07:26 -0500

    Add Flex story

---
+++ components/Flex/Flex.story.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Box from '../Box';
+import Flex, { FlexItem } from './';
+
+storiesOf('Flex', module).add('default', () => (
+  <Flex>
+    <FlexItem marginRight="base">
+      <Box width="200px" height="200px" backgroundColor="primary" />
+    </FlexItem>
+    <FlexItem>
+      <Box width="200px" height="200px" backgroundColor="primary" />
+    </FlexItem>
+  </Flex>
+));
