562bb47ce3390f133d7f34eca053fd401c39f99a
Drew Barontini <hello@drewb.io>
2018-12-12 16:34:04 -0500

    Update Flex component

--- components/Flex/Flex.js
+++ components/Flex/Flex.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { ifProp } from 'styled-tools';
 
@@ -23,5 +24,15 @@ const Flex = styled.div`
 `;
 
 Flex.displayName = 'Flex';
+
+Flex.propTypes = {
+  alignItems: PropTypes.string,
+  center: PropTypes.string,
+  flex: PropTypes.string,
+  flexDirection: PropTypes.string,
+  flexWrap: PropTypes.string,
+  height: PropTypes.string,
+  justifyContent: PropTypes.string,
+};
 
 export default Flex;
