0b051d0674f88dd8382760f27e846e9ba8e72f7c
Drew Barontini <hello@drewb.io>
2018-12-13 14:42:11 -0500

    Add Text tests

---
+++ components/Text/tests/Text.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Text from '../';
+
+describe('<Text />', () => {
+  it('should render properly', () => {
+    const { container } = render(<Text theme={theme}>My Text</Text>);
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule(
+      'font-size',
+      theme.fonts.sizes.base
+    );
+    expect(container.firstChild).toHaveTextContent('My Text');
+  });
+});
