16478d7cfe631fa0b46410572c304e788622fa98
Drew Barontini <hello@drewb.io>
2018-12-13 10:19:00 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// Config
+export { icons } from 'config/icons';
+export { theme } from 'config/theme';
+
+// Utilities
+export {
+  hover,
+  intToStyleUnit,
+  styleUnitMath,
+  styleUnitToInt,
+  palette,
+  setProp,
+  theme
+} from 'lib/utils';
+
+// Components
+export { A } from 'components/A';
+export { Avatar } from 'components/Avatar';
+export { Box } from 'components/Box';
+export { Button } from 'components/Button';
+export { Card } from 'components/Card';
+export { Checkbox } from 'components/Checkbox';
+export { Divider } from 'components/Divider';
+export { Flex, FlexItem } from 'components/Flex';
+export { Grid, GridItem } from 'components/Grid';
+export { Heading } from 'components/Heading';
+export { Icon } from 'components/Icon';
+export { Image } from 'components/Image';
+export { List } from 'components/List';
+export { Radio } from 'components/Radio';
+export { Select } from 'components/Select';
+export { Space } from 'components/Space';
+export { Table } from 'components/Table';
+export { Text } from 'components/Text';
+export { TextInput } from 'components/TextInput';
