87d6b282c9720a817f3ecacad93eb0521965edd4
Drew Barontini <hello@drewb.io>
2018-12-13 13:58:19 -0500

    Add Divider tests

---
+++ components/Divider/tests/Divider.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Divider from '../';
+
+describe('<Divider />', () => {
+  it('should render properly', () => {
+    const { container } = render(<Divider theme={theme} />);
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      theme.colors.subdued
+    );
+    expect(container.firstChild).toHaveStyleRule('height', '1px');
+    expect(container.firstChild).toHaveStyleRule('width', '100%');
+  });
+});
