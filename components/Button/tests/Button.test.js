5deb76cfcee70f835a563af9d2d0da5e944eb157
Drew Barontini <hello@drewb.io>
2018-12-13 14:54:42 -0500

    Update Button tests

--- components/Button/tests/Button.test.js
+++ components/Button/tests/Button.test.js
@@ -9,12 +9,9 @@ import Button from '../';
 describe('<Button />', () => {
   it('should render properly', () => {
     const { container } = render(<Button theme={theme}>My Button</Button>);
-    const { container: container2 } = render(
-      <Button primary theme={theme}>
-        My Button
-      </Button>
-    );
     expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild.tagName.toLowerCase()).toEqual('button');
+    expect(container.firstChild).toHaveTextContent('My Button');
     expect(container.firstChild).toHaveStyleRule(
       'background-color',
       'transparent'
@@ -24,15 +21,5 @@ describe('<Button />', () => {
       `2px solid ${theme.colors.fg}`
     );
     expect(container.firstChild).toHaveStyleRule('color', theme.colors.fg);
-    expect(container2.firstChild).toMatchSnapshot();
-    expect(container2.firstChild).toHaveStyleRule(
-      'background-color',
-      theme.colors.primary
-    );
-    expect(container2.firstChild).toHaveStyleRule(
-      'border',
-      '2px solid transparent'
-    );
-    expect(container2.firstChild).toHaveStyleRule('color', theme.colors.bg);
   });
 });
