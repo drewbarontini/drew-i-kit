a0965506eb222a7aaacb07463c553a5c97ab7370
Drew Barontini <hello@drewb.io>
2018-12-13 16:25:01 -0500

    Update distribution process

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// Config
+export { theme as defaultTheme } from './config';
+
+// Utilities
+export {
+  hover,
+  intToStyleUnit,
+  styleUnitMath,
+  styleUnitToInt,
+  palette,
+  setProp,
+  theme,
+} from './lib/utils';
+
+// Components
+export { default as A } from './components/A';
+export { default as Avatar } from './components/Avatar';
+export { default as Box } from './components/Box';
+export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
+export { default as Checkbox } from './components/Checkbox';
+export { default as Divider } from './components/Divider';
+export { default as Flex, default as FlexItem } from './components/Flex';
+export { default as GlobalStyles } from './components/GlobalStyles';
+export { default as Grid, default as GridItem } from './components/Grid';
+export { default as Heading } from './components/Heading';
+export { default as Icon } from './components/Icon';
+export { default as Image } from './components/Image';
+export { default as List } from './components/List';
+export { default as Radio } from './components/Radio';
+export { default as Select } from './components/Select';
+export { default as Space } from './components/Space';
+export { default as Table } from './components/Table';
+export { default as Text } from './components/Text';
+export { default as TextInput } from './components/TextInput';
