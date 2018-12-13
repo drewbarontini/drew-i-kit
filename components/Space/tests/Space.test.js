b7c1c6b56f09aa555e7947675481b12683aa9da5
Drew Barontini <hello@drewb.io>
2018-12-13 14:35:46 -0500

    Add Space tests

---
+++ components/Space/tests/Space.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Space from '../';
+
+describe('<Space />', () => {
+  it('should render properly', () => {
+    const { container, debug } = render(
+      <Space theme={theme} bottom="base">
+        Hello, Space!
+      </Space>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule(
+      'margin-bottom',
+      theme.spacing.base
+    );
+    expect(container.firstChild).toHaveTextContent('Hello, Space!');
+  });
+});
