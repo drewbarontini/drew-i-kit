f0e855e538a0c29be47eabda45223edd26d6e9ea
Drew Barontini <hello@drewb.io>
2018-12-13 14:49:44 -0500

    Update Avatar tests

--- components/Avatar/tests/Avatar.test.js
+++ components/Avatar/tests/Avatar.test.js
@@ -8,9 +8,11 @@ import Avatar from '../';
 
 describe('<Avatar />', () => {
   it('should render properly', () => {
-    const { container } = render(
+    const { container, debug } = render(
       <Avatar src="IMG_SRC" alt="Alt Text" width="50" height="50" />
     );
     expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveAttribute('height');
+    expect(container.firstChild).toHaveAttribute('width');
   });
 });
