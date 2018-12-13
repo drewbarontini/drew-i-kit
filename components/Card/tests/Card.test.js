2d89e1a324d4e1027a0e804644060edf29a38683
Drew Barontini <hello@drewb.io>
2018-12-13 14:56:38 -0500

    Update Card tests

--- components/Card/tests/Card.test.js
+++ components/Card/tests/Card.test.js
@@ -10,6 +10,8 @@ describe('<Card />', () => {
   it('should render properly', () => {
     const { container } = render(<Card theme={theme}>My Card</Card>);
     expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild.tagName.toLowerCase()).toEqual('div');
+    expect(container.firstChild).toHaveTextContent('My Card');
     expect(container.firstChild).toHaveStyleRule(
       'background-color',
       theme.colors.white
