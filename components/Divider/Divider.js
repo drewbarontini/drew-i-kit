8acbc5301d0ee4935cb5a3a89a6bbc289e350716
Drew Barontini <hello@drewb.io>
2018-12-13 15:46:46 -0500

    Update Divider

--- components/Divider/Divider.js
+++ components/Divider/Divider.js
@@ -1,31 +1,31 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { ifProp } from 'styled-tools';
 
-import { setProp } from '../../lib/utils';
+import { setProp, palette } from '../../lib/utils';
 
 const Divider = styled.div`
-  ${props =>
-    setProp({
-      prop: 'color',
-      cssProp: 'background-color',
-      themeKey: 'colors',
-      fallback: props.theme.colors.subdued,
-    })};
+  ${setProp({
+    prop: 'color',
+    cssProp: 'background-color',
+    themeKey: 'colors',
+    fallback: props => palette('subdued')(props),
+  })};
   ${setProp({
     prop: 'size',
-    cssProp: 'height',
+    cssProp: props => ifProp('vertical', 'width', 'height')(props),
     fallback: '1px',
   })};
   ${setProp({
     prop: 'marginBottom',
     themeKey: 'spacing',
-  })}
+  })};
   ${setProp({
     prop: 'marginTop',
     themeKey: 'spacing',
-  })}
+  })};
   ${setProp({
-    prop: 'width',
+    prop: props => ifProp('vertical', 'height', 'width')(props),
     fallback: '100%',
   })};
 `;
@@ -35,6 +35,7 @@ Divider.propTypes = {
   marginBottom: PropTypes.string,
   marginTop: PropTypes.string,
   size: PropTypes.string,
+  vertical: PropTypes.bool,
   width: PropTypes.string,
 };
 
--- components/Divider/Divider.story.js
+++ components/Divider/Divider.story.js
@@ -5,6 +5,7 @@ import Divider from './';
 
 storiesOf('Divider', module)
   .add('default', () => <Divider />)
+  .add('vertical', () => <Divider vertical height="50px" />)
   .add('size', () => <Divider size="4px" />)
   .add('color', () => <Divider size="4px" color="primary" />)
   .add('width', () => <Divider size="4px" color="primary" width="50px" />);
