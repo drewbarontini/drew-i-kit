90546597d86ec2ae35c888e24b7f33e272ee877e
Drew Barontini <hello@drewb.io>
2018-12-13 14:27:56 -0500

    Add Radio tests

---
+++ components/Radio/tests/Radio.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Radio from '../';
+
+describe('<A />', () => {
+  it('should render properly', () => {
+    const { container, debug } = render(<Radio name="radio" theme={theme} />);
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveAttribute('id');
+    expect(container.firstChild.id).toEqual('radio');
+    expect(container.firstChild).toHaveAttribute('name');
+    expect(container.firstChild.name).toEqual('radio');
+    expect(container.firstChild).toHaveAttribute('type');
+    expect(container.firstChild.type).toEqual('radio');
+  });
+});
