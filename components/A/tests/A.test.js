ef60c73e4133494158a1f302328d3542b11d64e3
Drew Barontini <hello@drewb.io>
2018-12-13 12:28:02 -0500

    Add A tests

---
+++ components/A/tests/A.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import theme from '../../../config/theme';
+
+import A from '../';
+
+describe('<A />', () => {
+  it('should work', () => {
+    const { container } = render(
+      <A href="#0" theme={theme}>
+        My Link
+      </A>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule('color', theme.colors.primary);
+    expect(container.firstChild).toHaveStyleRule('text-decoration', 'none');
+  });
+});
