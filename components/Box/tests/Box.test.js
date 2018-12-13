ceebe552f8ff94d0e5b7b7c11a5e668f4520d793
Drew Barontini <hello@drewb.io>
2018-12-13 12:29:17 -0500

    Add Box tests

---
+++ components/Box/tests/Box.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import theme from '../../../config/theme';
+
+import Box from '../';
+
+describe('<Box />', () => {
+  it('should match snapshot', () => {
+    const { container } = render(
+      <Box backgroundColor="primary" padding="base" theme={theme} />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      theme.colors.primary
+    );
+    expect(container.firstChild).toHaveStyleRule('padding', theme.spacing.base);
+  });
+});
