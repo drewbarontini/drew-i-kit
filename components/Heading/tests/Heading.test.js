ce4be121ba6c59ebd80079fa104c3fb2b3426957
Drew Barontini <hello@drewb.io>
2018-12-13 14:03:27 -0500

    Add Heading tests

---
+++ components/Heading/tests/Heading.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Heading from '../';
+
+describe('<Heading />', () => {
+  it('should render properly', () => {
+    const { container } = render(<Heading theme={theme}>My Heading</Heading>);
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule(
+      'font-size',
+      theme.fonts.sizes.h1
+    );
+  });
+});
