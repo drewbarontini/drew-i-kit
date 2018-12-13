0b40a611a8a56600141b70b58b13f6230542c8fe
Drew Barontini <hello@drewb.io>
2018-12-13 14:58:16 -0500

    Add Checkbox tests

---
+++ components/Checkbox/tests/Checkbox.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Checkbox from '../';
+
+describe('<Checkbox />', () => {
+  it('should render properly', () => {
+    const { container } = render(<Checkbox name="checkbox" />);
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild.tagName.toLowerCase()).toEqual('input');
+    expect(container.firstChild).toHaveAttribute('id');
+    expect(container.firstChild.id).toEqual('checkbox');
+    expect(container.firstChild).toHaveAttribute('name');
+    expect(container.firstChild.name).toEqual('checkbox');
+    expect(container.firstChild).toHaveAttribute('type');
+    expect(container.firstChild.type).toEqual('checkbox');
+  });
+});
