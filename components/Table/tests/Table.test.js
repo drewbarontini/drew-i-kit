1fc9a3d570a3ff2070f5c607548b44d4ee565f1e
Drew Barontini <hello@drewb.io>
2018-12-13 14:40:34 -0500

    Add Table tests

---
+++ components/Table/tests/Table.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import { theme } from '../../../config';
+
+import Table from '../';
+
+describe('<Table />', () => {
+  it('should render properly', () => {
+    const { container } = render(
+      <Table
+        theme={theme}
+        headings={[{ key: 'foo', title: 'Foo' }, { key: 'bar', title: 'Bar' }]}
+        data={[{ foo: 'Hello', bar: 'Table' }]}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyleRule('border-collapse', 'collapse');
+    expect(container.firstChild).toHaveStyleRule('width', '100%');
+    expect(container.firstChild.tagName).toEqual('TABLE');
+  });
+});
