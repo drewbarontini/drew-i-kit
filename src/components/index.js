bb66e16f582d8f8c55e475aef77c6ad07800a2dc
Drew Barontini <hello@drewb.io>
2018-12-14 12:12:23 -0500

    Export withStyledComponents

--- src/components/index.js
+++ src/components/index.js
@@ -19,3 +19,4 @@ export { default as Space } from './Space';
 export { default as Table } from './Table';
 export { default as Text } from './Text';
 export { default as TextInput } from './TextInput';
+export { default as withStyledComponents } from './withStyledComponents';
