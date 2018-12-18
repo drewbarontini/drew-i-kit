b4eab6e824619792d76f31da501a2245d191e7ca
Drew Barontini <hello@drewb.io>
2018-12-18 15:10:29 -0500

    Move global styles into own file

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -1,36 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-import { palette, theme, responsive } from '../../lib';
+import globalStyles from '../../config/globalStyles';
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  body {
-    background-color: ${palette('bg')};
-    color: ${palette('fg')};
-    font-family: ${theme('fonts.family.base')};
-    font-size: ${theme('fonts.sizes.base')};
-    line-height: ${theme('fonts.lineHeight.base')};
-    margin: ${theme('spacing.l')};
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    margin: 0;
-    margin-bottom: ${theme('spacing.s')};
-  }
-
-  img {
-    display: block;
-    max-width: 100%;
-  }
-
-  ${responsive};
+  ${globalStyles};
 `;
 
 GlobalStyles.displayName = 'GlobalStyles';
---
+++ src/config/globalStyles.js
@@ -0,0 +1,42 @@
+import { css } from 'styled-components';
+
+import { palette, theme } from '../lib/props';
+import { responsive } from '../lib/responsive';
+
+const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: ${palette('bg')};
+    color: ${palette('fg')};
+    font-family: ${theme('fonts.family.base')};
+    font-size: ${theme('fonts.sizes.base')};
+    line-height: ${theme('fonts.lineHeight.base')};
+    margin: ${theme('spacing.l')};
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    margin-bottom: ${theme('spacing.s')};
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  ${responsive};
+`;
+
+export default globalStyles;
